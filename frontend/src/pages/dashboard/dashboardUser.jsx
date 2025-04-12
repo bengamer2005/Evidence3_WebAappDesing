c33067ef73cf5c2d6f87ae6bdd2c04dd43b28534
bengamer2005 <garciabenitoleonel@gmail.com>
2025-04-12 12:23:03 -0600

    cambios: -card.jsx: modifique el componente para las cards, agregeando su estructura y tambien su css. tambien le agregue porps para agregarle los links, imgs, titulo y descripciones a cada card desde los dashboards -dashboards: les di los valor a los props para cada dashboard

--- frontend/src/pages/dashboard/dashboardUser.jsx
+++ frontend/src/pages/dashboard/dashboardUser.jsx
@@ -7,11 +7,35 @@ const DashboardUser = () => {
         <>
             <main>
                 <Header/>
+                <h1>WELCOME Users</h1>
                 <div className="cards">
-                    <Card role = "SALES"></Card>
-                    <Card role = "PURCHASING"></Card>
-                    <Card role = "WAREHOUSE"></Card>
-                    <Card role = "ROUTE"></Card>
+                    <Card 
+                        link = "/halcon-user/dashboard-user/sales"
+                        img = "https://st3.depositphotos.com/1001335/14944/i/450/depositphotos_149444160-stock-illustration-materials-for-construction-3d-illustration.jpg"
+                        role = "SALES" 
+                        description = "Aqui estara la descripcion de sales">    
+                    </Card>
+
+                    <Card 
+                        link = "/halcon-user/dashboard-user/purchasing"
+                        img = "https://images.adsttc.com/media/images/5ea0/a3ce/b357/65f0/d600/00d8/newsletter/shutterstock_1185452806.jpg?1587585977"
+                        role = "PURCHASING" 
+                        description = "Aqui estara la descripcion de purchasing">
+                    </Card>
+
+                    <Card 
+                        link = "/halcon-user/dashboard-user/warehouse"
+                        img = "https://www.microtech.es/hubfs/Fotos%20blog/automatizacion_compras_materiales-1.jpg"
+                        role = "WAREHOUSE" 
+                        description = "Aqui estara la descripcion warehouse">
+                    </Card>
+
+                    <Card 
+                        link = "/halcon-user/dashboard-user/route"
+                        img = "https://img.freepik.com/fotos-premium/camion-carroceria-totalmente-cargada-bloques-hormigon-cemento_165577-3056.jpg"
+                        role = "ROUTE" 
+                        description = "Aqui estara la descripcion route">
+                    </Card>
                 </div>
             </main>
         </>
