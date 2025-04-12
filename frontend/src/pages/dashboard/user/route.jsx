04418877f8025be4038035ff17bdd1439050b2fd
bengamer2005 <garciabenitoleonel@gmail.com>
2025-04-12 17:10:39 -0600

    agrege el apartado para los roles de users, tambien los llame en App.jsx, falta: -form: agregar sus respectivos forms para agregar/mandar o cambiar status de las ordenes -orders: mostrar las ordenes por medio de mis endpoints

---
+++ frontend/src/pages/dashboard/user/route.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../../../components/header"
+
+const RouteUser = () => {
+    return (
+        <>
+            <main>
+                <Header/>
+                <h1>ROUTE</h1>
+                <p>este sera el apartado de warehouse, falta: ver ordenes, form</p>
+            </main>
+        </>
+    )
+}
+
+export default RouteUser
---
+++ frontend/src/pages/dashboard/user/sales.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../../../components/header"
+
+const Sales = () => {
+    return (
+        <>
+            <main>
+                <Header/>
+                <h1>SALES</h1>
+                <p>este sera el apartado de sales, falta: ver ordenes, form</p>
+            </main>
+        </>
+    )
+}
+
+export default Sales
